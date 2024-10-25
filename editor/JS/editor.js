d8e088f52b867eace1775856e15798e3407bbce6
Heyctoi <ayoub.khloufi@condorcet.be>
2024-10-25 17:52:04 +0200

    Ayoub

--- editor/JS/editor.js
+++ editor/JS/editor.js
@@ -10,8 +10,6 @@ function init(){
     $("submitWord").onclick = EditWord;
     $("submitObj").onclick = EditObj;
     $("submitSave").onclick = EditSave;
-    hide("AddMsg");
-    hide("AddObj");
     GetMaxValue();
 }
 
@@ -32,33 +30,42 @@ function ShowObject(){
     hide("AddMsg");
     show("AddObj");
 }
+function Add(url,param,element){
+    var xmlHttpRequest= new XMLHttpRequest();
+    xmlHttpRequest.open("POST",url,true);
+    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+    xmlHttpRequest.send(param);
+}
+
 
 function AddRoom(){
     var roomName = $("RoomValue").value;
     var url="PHP/AddRoom.php";
     var param="room="+encodeURIComponent(roomName);
-    var xmlHttpRequest= new XMLHttpRequest();
-    xmlHttpRequest.open("POST",url,true);
-    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xmlHttpRequest.send(param);
-
+    Add(url,param,roomName);
 }
 
 function AddMessage(){
     var msgName = $("MsgValue").value;
     var url="PHP/AddMsg.php";
     var param="msg="+encodeURIComponent(msgName);
-    var xmlHttpRequest= new XMLHttpRequest();
-    xmlHttpRequest.open("POST",url,true);
-    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xmlHttpRequest.send(param);
+    Add(url,param,msgName);
 }
 
 function AddObject(){
     var objName = $("ObjValue").value;
     var url="PHP/AddObj.php";
     var param="obj="+encodeURIComponent(objName);
+    Add(url,param,objName);
+}
+
+function Edit(url,param){
     var xmlHttpRequest= new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function(){
+        if (xmlHttpRequest.readyState === 4 && xmlHttpRequest.status === 200){
+            GetMaxValue();
+        }
+    }
     xmlHttpRequest.open("POST",url,true);
     xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
     xmlHttpRequest.send(param);
@@ -69,11 +76,7 @@ function EditWord(){
     console.log(wordSize);
     var url="PHP/EditWord.php";
     var param="wordSize="+encodeURIComponent(wordSize);
-    var xmlHttpRequest= new XMLHttpRequest();
-    xmlHttpRequest.open("POST",url,true);
-    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xmlHttpRequest.send(param);
-    location.reload();
+    Edit(url,param);
 }
 
 function EditObj(){
@@ -81,22 +84,14 @@ function EditObj(){
     console.log(maxObj);
     var url="PHP/EditObj.php";
     var param="maxObj="+encodeURIComponent(maxObj);
-    var xmlHttpRequest= new XMLHttpRequest();
-    xmlHttpRequest.open("POST",url,true);
-    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xmlHttpRequest.send(param);
-    location.reload();
+    Edit(url,param);
 }
 
 function EditSave(){
     var maxSave = $("maxSave").value;
     var url="PHP/EditSave.php";
     var param="maxSave="+encodeURIComponent(maxSave);
-    var xmlHttpRequest= new XMLHttpRequest();
-    xmlHttpRequest.open("POST",url,true);
-    xmlHttpRequest.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xmlHttpRequest.send(param);
-    location.reload();
+    Edit(url,param);
 }
 
 function GetMaxValue(){
@@ -116,7 +111,7 @@ function SplitValue(response){
     var wordSize_p = $("wordSize_p");
     var maxObj_p = $("maxObj_p");
     var maxSave_p = $("maxSave_p");
-    wordSize_p.textContent+=split[2];
     maxObj_p.textContent+=split[0];
     maxSave_p.textContent+=split[1];
+    wordSize_p.textContent+=split[2];
 }
